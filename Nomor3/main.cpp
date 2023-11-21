10d35808137524e08f4eea2e2823e8c284a1373d
nania <azzahranania04@gmail.com>
2023-11-20 20:40:21 -0800

    Praktikum 5

---
+++ Nomor3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+ //Fungsi menghitung Deret Fibonacci
+ int fibonacci(int n) {
+  if (n == 0 || n ==1){
+    return n;
+  } else {
+    return (fibonacci(n-1) + fibonacci(n-2));
+  }
+}
+
+int main(){
+    int jumlahAngka;
+
+  cout << "Program Deret Fibonacci" << endl;
+
+  int n, m, e= 0;
+
+    cout << "Jumlah Deret Fibonacci yang dibutuhkan : "; cin >> n;
+ 	cout << "Hasil Bilangan Fibonacci : \n";
+ 		 for (m = 1; m <= n; m++){
+    		cout << "Fibonacci ke - "<< m <<" : " << fibonacci(e) << endl;
+   		 e++;
+ 		 }
+
+  cout << endl;
+    return 0;
+}
